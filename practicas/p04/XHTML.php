89901f180aa43614cabf6dbc13b08c9036a18546
maguis18 <rt202353021@alm.buap.mx>
2025-02-04 20:18:51 -0600

    Ejercicio 2 agregado al archivo XHTML.php

--- practicas/p04/XHTML.php
+++ practicas/p04/XHTML.php
@@ -36,5 +36,47 @@
         unset($var7);
         unset($_element1);
 ?>
+
+<h2>Ejercicio 2</h2>
+    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
+    <p>
+    $a = "ManejadorSQL";
+    $b = 'MySQL';
+    $c = &$a;
+    </p>
+    <?php
+        $a = "ManejadorSQL";
+        $b = 'MySQL';
+        $c = &$a;
+
+        echo '<h4>Respuesta:</h4>'; 
+        echo '<ul>';
+        echo '<li>$a = ' . $a . '</li>';
+        echo '<li>$b = ' . $b . '</li>';
+        echo '<li>$c = ' . $c . '</li>';
+        echo '</ul>';
+        $a = "PHP server";
+        $b = &$a;
+
+        echo '<h4>Paso 2:</h4>
+        <ul><li> a. Ahora muestra el contenido de cada variable </li>
+        <li>b. Agrega al código actual las siguientes asignaciones:</li>
+        <li>c. Vuelve a mostrar el contenido de cada uno</li>
+        </ul>';
+        echo '<h4>Respuesta:</h4>'; 
+        echo '<ul>';
+        echo '<li>$a = ' . $a . '</li>';
+        echo '<li>$b = ' . $b . '</li>';
+        echo '<li>$c = ' . $c . '</li>';
+        echo '</ul>';
+
+        unset($a);
+        unset($b);
+        unset($c);
+    ?>
+        <h4>¿Qué ocurrio en el segundo bloque de asignaciones?</h4>
+        <p>
+        Se imprimieron en las variables a, b y c lo mismo , el texto "PHP server" ya que se le asigno a la variable $a el texto de "PHP server" , despues a la variable b se le asigno una referencia a lo que contiene a , y desde un inicio la variable c tenia como valor la referencia a la variable a, por lo que al no reasignarle ningun valor, sigue imprimiendo la referencia al valor de a, que en este punto era el texto "PHP server"</p>   
+
 </body>
 </html>
