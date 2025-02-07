5e93cd8f9b3acd81928c123cbed769a046efaebe
maguis18 <rt202353021@alm.buap.mx>
2025-02-06 22:11:13 -0600

    llamado a la funcion del ejercicio 3 y 4 agregado

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -29,6 +29,61 @@
     secuencia();
 ?>
 
+<H2>Ejercicio3</H2>
+<p>Utiliza un ciclo while para encontrar el primer número entero obtenido aleatoriamente,
+pero que además sea múltiplo de un número dado.</p>
+<ul>
+    <li>
+    Crear una variante de este script utilizando el ciclo do-while.
+    </li>
+    <li>
+    El número dado se debe obtener vía GET.
+    </li>
+</ul>
+<h4>usando while</h4>
+<?php
+    require_once __DIR__ . '/src/funciones.php';
+    if(isset($_GET['numero']))
+    {
+    primer_numero($_GET['numero']);
+    }
+?>
+<h4>usando do while</h4>
+<?php
+    require_once __DIR__ . '/src/funciones.php';
+    if(isset($_GET['numero']))
+    {
+    primer_numero_do_while($_GET['numero']);
+    }
+?>
+<h2>Ejercicio 4</h2>
+<p>Crear un arreglo cuyos índices van de 97 a 122 y cuyos valores son las letras de la ‘a’
+a la "z". Usa la función chr(n) que devuelve el caracter cuyo código ASCII es n para poner
+el valor en cada índice. Es decir:</p>
+<p>[97] => a
+    <br>
+[98] => b
+<br>
+[99] => c
+<br>
+...
+<br>
+[122] => z
+</p>
+<ul>
+    <li>Crea el arreglo con un ciclo for</li>
+    <li>Lee el arreglo y crea una tabla en XHTML con echo y un ciclo foreach
+foreach ($arreglo as $key => $value) {
+# code...
+} </li>
+</ul>
+
+<?php
+    require_once __DIR__ . '/src/funciones.php';
+    abecedario();
+?>
+
+
 
 </body>
 </html>
