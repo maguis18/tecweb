fed4c722653ded1ef68c7086fc72613f58a23a4d
maguis18 <rt202353021@alm.buap.mx>
2025-02-06 22:08:01 -0600

    funcion ejercicio 2 agregada a funciones

--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -11,4 +11,29 @@ function es_multiplo7y5($num)
         echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
     }
 }
+
+function secuencia(){
+    $secuencia = [];
+    $i = 0;
+    
+    function esPar($numero) {
+        return $numero % 2 == 0;
+    }
+    
+    while (true) {
+        $i++;
+        $n1 = rand(1, 999); 
+        $n2 = rand(1, 999);
+        $n3 = rand(1, 999);
+    
+        if (!esPar($n1) && esPar($n2) && !esPar($n3)) {
+            $total = $i * 3;
+            echo "<H3>R= " . $total . " números obtenidos en " . $iteraciones . " iteraciones.\n</H3>";
+            break; 
+        } else {
+            $secuencia[$i] = [$n1, $n2, $n3];
+        }
+    }
+} 
+
 ?>
