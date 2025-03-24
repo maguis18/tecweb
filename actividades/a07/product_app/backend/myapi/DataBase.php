9e36ad4365c1c8dfae18a8abfdb3e43e9746f109
maguis18 <rt202353021@alm.buap.mx>
2025-03-23 23:04:30 -0600

    clase abstracta creada

---
+++ actividades/a07/product_app/backend/myapi/DataBase.php
@@ -0,0 +1,16 @@
+<?php
+abstract class DataBase{
+    protected $conexion;
+
+    public function __construct($db, $user, $pass){
+        $this->conexion = new mysqli('localhost', $user, $pass, $db);
+
+        // Verificar si la conexión fue exitosa
+        if ($this->conexion->connect_error) {
+            die('Conexión fallida: ' . $this->conexion->connect_error);
+        }
+    }
+
+}
+
+?>
