056d426500204259def6aaac402051cd45e8859f
maguis18 <rt202353021@alm.buap.mx>
2025-04-01 23:51:03 -0600

    Creacion del controlador, se juntaron todos los archivos php que se usaban antes

---
+++ actividades/product_app_MVC/backend/controllers/ProductController.php
@@ -0,0 +1,48 @@
+<?php
+require_once '../models/products.php';
+
+use tec_Web\myapi\Products as Products;
+
+// Instancia del modelo
+$product = new Products('marketzone');
+
+// Obtén la acción a realizar a partir de un parámetro 'action'
+$action = isset($_GET['action']) ? $_GET['action'] : '';
+
+switch ($action) {
+    case 'list':
+        $product->list();
+        echo $product->getData();
+        break;
+    case 'add':
+        // Se espera que los datos lleguen vía POST
+        $jsonOBJ = json_decode(json_encode($_POST));
+        echo $product->add($jsonOBJ);
+        break;
+    case 'delete':
+        $jsonOBJ = json_decode(json_encode($_POST));
+        echo $product->delete($jsonOBJ->id);
+        break;
+    case 'edit':
+        $jsonOBJ = json_decode(json_encode($_POST));
+        echo $product->update($jsonOBJ);
+        break;
+    case 'search':
+        echo $product->search();
+        break;
+    case 'single':
+        $jsonOBJ = json_decode(json_encode($_POST));
+        echo $product->single($jsonOBJ->id);
+        break;
+    case 'singleByName':
+        if(isset($_GET['name'])){
+            echo $product->singleByName($_GET['name']);
+        } else {
+            echo json_encode(['status'=>'error','message'=>'Parámetro "name" no proporcionado']);
+        }
+        break;
+    default:
+        echo json_encode(['status'=>'error','message'=>'Acción no definida']);
+        break;
+}
+?>
