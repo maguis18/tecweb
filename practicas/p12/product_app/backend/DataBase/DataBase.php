7c383c59a94d46a0c77731125acf2f7b11bc734a
maguis18 <rt202353021@alm.buap.mx>
2025-04-03 22:12:37 -0600

    se creo la carpeta para DataBase

---
+++ practicas/p12/product_app/backend/DataBase/DataBase.php
@@ -0,0 +1,29 @@
+<?php
+namespace TECWEB\MYAPI;
+//considerar si se pasa como default "marketzone"
+abstract class DataBase {
+    protected $conexion;
+    protected $data;
+
+    public function __construct($db, $user, $pass) {
+        $this->conexion = @mysqli_connect(
+            'localhost',
+            $user,
+            $pass,
+            $db
+        );
+    
+        /**
+         * NOTA: si la conexión falló $conexion contendrá false
+         **/
+        if(!$this->conexion) {
+            die('¡Base de datos NO conextada!');
+        }
+        $this->data = [];
+    }
+    public function getData() {
+        // SE HACE LA CONVERSIÓN DE ARRAY A JSON
+        return json_encode($this->data, JSON_PRETTY_PRINT);
+    }
+}
+?>
