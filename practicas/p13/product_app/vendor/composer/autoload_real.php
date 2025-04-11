8d9ee50f8fe7fe9474dbb063018e762288aa298b
maguis18 <rt202353021@alm.buap.mx>
2025-04-10 19:38:06 -0600

    Instalacion de slim completada

--- practicas/p13/product_app/vendor/composer/autoload_real.php
+++ practicas/p13/product_app/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit5448e05a5a64fe842856604fbc420654
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit5448e05a5a64fe842856604fbc420654', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit5448e05a5a64fe842856604fbc420654', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit5448e05a5a64fe842856604fbc420654
         call_user_func(\Composer\Autoload\ComposerStaticInit5448e05a5a64fe842856604fbc420654::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit5448e05a5a64fe842856604fbc420654::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
