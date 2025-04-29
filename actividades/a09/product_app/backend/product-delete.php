429c4b5efebab573b79742b8ed2d1f258e45d6c1
maguis18 <rt202353021@alm.buap.mx>
2025-04-29 17:18:53 -0600

    actividad 09 agregada

---
+++ actividades/a09/product_app/backend/product-delete.php
@@ -0,0 +1,7 @@
+<?php
+    require_once __DIR__.'/../vendor/autoload.php';
+    use TECWEB\BACKEND\Delete\Delete;
+    $productos = new Delete('marketzone');
+    $productos->delete( $_POST['id'] );
+    echo $productos->getData();
+?>
